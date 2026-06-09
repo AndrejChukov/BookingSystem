3f8c533ddd96747e21b61681f84b667b40c1c287
Andrej <andrej_chuchkalov@mail.ru>
2026-06-09 17:15:19 +0300

    feat(mapper): add updateEntityFromDto method

--- src/main/java/ru/bookingsystem/mapper/RoomMapper.java
+++ src/main/java/ru/bookingsystem/mapper/RoomMapper.java
@@ -1,10 +1,16 @@
 package ru.bookingsystem.mapper;
 
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 import ru.bookingsystem.dto.request.RoomRequestDTO;
 import ru.bookingsystem.entity.Room;
 
 @Mapper(componentModel = "spring", uses = {EquipmentMapper.class})
 public interface RoomMapper {
     Room toEntity(RoomRequestDTO roomRequest);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateEntityFromDto(RoomRequestDTO roomRequest, @MappingTarget Room entity);
 }
