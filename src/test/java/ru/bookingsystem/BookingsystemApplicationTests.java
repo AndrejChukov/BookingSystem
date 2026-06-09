e3cfa601ef4895428d6d9efc2462f3cdfa45bc51
Andrej <andrej_chuchkalov@mail.ru>
2026-06-09 15:52:05 +0300

    fix(BookingsystemApplicationTest): connect all tests

--- src/test/java/ru/bookingsystem/BookingsystemApplicationTests.java
+++ src/test/java/ru/bookingsystem/BookingsystemApplicationTests.java
@@ -3,7 +3,7 @@ package ru.bookingsystem;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = BookingsystemApplication.class)
 class BookingsystemApplicationTests {
 
     @Test
