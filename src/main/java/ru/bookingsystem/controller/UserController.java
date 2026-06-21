d1dbecb4ff674df0b18acaaa555c8fe048a20ab9
Andrej <andrej_chuchkalov@mail.ru>
2026-06-21 16:56:07 +0300

    feat(UserController and Serivce): add basic register and signin using jwt

--- src/main/java/ru/bookingsystem/controller/UserController.java
+++ src/main/java/ru/bookingsystem/controller/UserController.java
@@ -1,4 +1,30 @@
 package ru.bookingsystem.controller;
 
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.bookingsystem.dto.request.UserRequestDTO;
+import ru.bookingsystem.dto.response.UserResponseDTO;
+import ru.bookingsystem.service.UserService;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/auth/register")
+    public UserResponseDTO register(@Valid @RequestBody UserRequestDTO userRequest) {
+        return userService.register(userRequest);
+    }
+
+    @PostMapping("/auth/signin")
+    public UserResponseDTO authenticate(@Valid @RequestBody UserRequestDTO userRequest) {
+        return userService.authenticate(userRequest);
+    }
+
 }
--- src/main/java/ru/bookingsystem/service/UserService.java
+++ src/main/java/ru/bookingsystem/service/UserService.java
@@ -1,4 +1,55 @@
 package ru.bookingsystem.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import ru.bookingsystem.dto.request.UserRequestDTO;
+import ru.bookingsystem.dto.response.UserResponseDTO;
+import ru.bookingsystem.entity.User;
+import ru.bookingsystem.mapper.UserMapper;
+import ru.bookingsystem.repository.UserRepository;
+import ru.bookingsystem.security.TokenService;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
+
+    private final AuthenticationManager authenticationManager;
+    private final TokenService tokenService;
+
+    public UserResponseDTO register(UserRequestDTO userRequest) {
+        User newUser = userMapper.toEntity(userRequest);
+
+        newUser.setPassword(passwordEncoder.encode(userRequest.password()));
+        newUser.setRole(User.Role.USER);
+
+        userRepository.save(newUser);
+
+        String jwtToken = tokenService.generateToken(new UsernamePasswordAuthenticationToken(
+                newUser,
+                null,
+                newUser.getAuthorities()
+        ));
+
+        return new UserResponseDTO(jwtToken, newUser.getUsername(), newUser.getEmail());
+    }
+
+    public UserResponseDTO authenticate(UserRequestDTO userRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        userRequest.username(), userRequest.password()
+                )
+        );
+        User authUser = (User) authentication.getPrincipal();
+
+        String jwtToken = tokenService.generateToken(authentication);
+        return new UserResponseDTO(jwtToken, authUser.getUsername(), authUser.getEmail());
+    }
 }
