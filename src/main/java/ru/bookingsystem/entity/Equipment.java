9d4a47b073bdbb8dccda8ae412f1c48838f2cf1d
Andrej <andrej_chuchkalov@mail.ru>
2026-07-05 13:05:47 +0400

    fix: change @Data to @Getter/@Setter and hashcode, remove password from toString

--- src/main/java/ru/bookingsystem/entity/Equipment.java
+++ src/main/java/ru/bookingsystem/entity/Equipment.java
@@ -3,15 +3,21 @@ package ru.bookingsystem.entity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.Instant;
 
-@Data
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 @Table(name = "equipments")
 public class Equipment extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Long id;
     private String name;
 }
